170911dbb9961da86041bcc95ef50833c20fd5a5
Junki Kizuka <k.falconws@gmail.com>
2015-01-20 23:48:29 +0900

    modify web-mode.el settings, choice Emacs dark color theme.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -70,25 +70,36 @@
 ;; web-mode
 (require 'web-mode)
 
-;;; emacs 23以下の互換
-(when (< emacs-major-version 24)
-  (defalias 'prog-mode 'fundamental-mode))
-
 ;;; 適用する拡張子
-(add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsp$"       . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
-;;; インデント数
+;;; インデント関係
 (defun web-mode-hook ()
   "Hooks for Web mode."
-  (setq web-mode-html-offset   2)
-  (setq web-mode-css-offset    2)
-  (setq web-mode-script-offset 2)
-  (setq web-mode-php-offset    2)
-  (setq web-mode-java-offset   2)
-  (setq web-mode-asp-offset    2))
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+;;; auto indentation on
+  (local-set-key (kbd "RET") 'newline-and-indent)
+  (setq web-mode-attr-indent-offset 2))
 (add-hook 'web-mode-hook 'web-mode-hook)
+
+;;; Autoremove final white spaces on save
+(add-hook 'local-write-file-hooks
+            (lambda ()
+               (delete-trailing-whitespace)
+               nil))
+
+(setq web-mode-enable-css-colorization t)
+(setq web-mode-enable-current-element-highlight t)
+
+;;; カラーテーマ設定
+;;; http://aoe-tk.hatenablog.com/entry/20130210/1360506829
+(load-theme 'manoj-dark t)
