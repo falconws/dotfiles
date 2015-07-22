9bdd2f9d04b4bb23584a09dc4f515a2a9de392ea
Kizuka Junki <k.falconws@gmail.com>
2015-07-22 12:17:18 +0900

    Tab, 行末等を可視化

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -106,3 +106,20 @@
 ;;; カラーテーマ設定
 ;;; http://aoe-tk.hatenablog.com/entry/20130210/1360506829
 (load-theme 'manoj-dark t)
+
+;;
+;; whitespace
+;;
+(require 'whitespace)
+(setq whitespace-style '(face           ; faceで可視化
+                         trailing       ; 行末
+                         tabs           ; タブ
+                         empty          ; 先頭/末尾の空行
+                         space-mark     ; 表示のマッピング
+                         tab-mark
+                         ))
+
+(setq whitespace-display-mappings
+      '((tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
+
+(global-whitespace-mode 1)
