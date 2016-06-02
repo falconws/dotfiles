930c2f8324c231d07b6d40274b786c0fe7ce270a
Kizuka Junki <k.falconws@gmail.com>
2016-06-02 15:00:43 +0900

    fix package-archives repositorys

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,9 +17,9 @@
 ;; (init-loader-load "~/.emacs.d/conf")
 
 (require 'package)
-(add-to-list 'package-archives
-	     '("marmalade" . "http://marmalade-repo.org/packages/")
-	     '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
 ;; auto-complete.el
 (require 'auto-complete)
