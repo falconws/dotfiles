7f4fd34205e0e198f51f05f2d8bc4c71202d9051
Kizuka Junki <k.falconws@gmail.com>
2016-06-17 18:36:25 +0900

    add auto comment

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -144,3 +144,17 @@
 (require 'flymake-easy)
 (require 'flymake-python-pyflakes)
 (add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yaml-mode web-mode php-mode markdown-mode js2-mode flymake-python-pyflakes auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
